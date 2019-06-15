f2956a425288d514c6267271d958895d0374820a
que2017 <1563645183@qq.com>
2019-06-15 20:59:01 +0800

    完成设置闹钟功能

--- app/src/main/java/com/duiyi/androidclock/views/AlarmView.java
+++ app/src/main/java/com/duiyi/androidclock/views/AlarmView.java
@@ -1,5 +1,7 @@
 package com.duiyi.androidclock.views;
 
+import android.annotation.SuppressLint;
+import android.app.TimePickerDialog;
 import android.content.Context;
 import android.support.annotation.Nullable;
 import android.util.AttributeSet;
@@ -8,9 +10,11 @@ import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.LinearLayout;
 import android.widget.ListView;
+import android.widget.TimePicker;
 
 import com.duiyi.androidclock.R;
 
+import java.util.Calendar;
 import java.util.Date;
 
 /**
@@ -21,6 +25,8 @@ import java.util.Date;
  */
 public class AlarmView extends LinearLayout {
     private static final String TAG = AlarmView.class.getSimpleName();
+
+    private static final int ONE_DAY_MILLIS = 24 * 60 * 60 * 1000;
 
     private ListView mAlarmListView;
 
@@ -47,17 +53,39 @@ public class AlarmView extends LinearLayout {
         mAlarmListView = findViewById(R.id.alarmList);
         mAddAlarm = findViewById(R.id.addAlarm);
 
-        mAdapter = new ArrayAdapter<AlarmData>(getContext(), android.R.layout.simple_list_item_1);
+        mAdapter = new ArrayAdapter<>(getContext(), android.R.layout.simple_list_item_1);
         mAlarmListView.setAdapter(mAdapter);
-
-        mAdapter.add(new AlarmData(System.currentTimeMillis()));
 
         mAddAlarm.setOnClickListener(new OnClickListener() {
             @Override
             public void onClick(View v) {
-                // TODO 添加闹钟
+                addAlarm();
             }
         });
+    }
+
+    private void addAlarm() {
+        Calendar calendar = Calendar.getInstance();
+        new TimePickerDialog(getContext(), new TimePickerDialog.OnTimeSetListener() {
+            @Override
+            public void onTimeSet(TimePicker view, int hourOfDay, int minute) {
+                Calendar cale = Calendar.getInstance();
+                cale.set(Calendar.HOUR_OF_DAY, hourOfDay);
+                cale.set(Calendar.MINUTE, minute);
+
+                Calendar currentTime = Calendar.getInstance();
+
+                if (cale.getTimeInMillis() <= currentTime.getTimeInMillis()) {
+                    cale.setTimeInMillis(cale.getTimeInMillis() + ONE_DAY_MILLIS);
+                }
+
+                mAdapter.add(new AlarmData(cale.getTimeInMillis()));
+                saveAlarmList();
+            }
+        }, calendar.get(Calendar.HOUR_OF_DAY), calendar.get(Calendar.MINUTE), true).show();
+    }
+
+    private void saveAlarmList() {
     }
 
     private static class AlarmData {
@@ -65,12 +93,18 @@ public class AlarmView extends LinearLayout {
 
         private long time;
 
-        private Date date;
+        private Calendar calendar;
 
+        @SuppressLint("DefaultLocale")
         public AlarmData(long time) {
             this.time = time;
-            date = new Date(this.time);
-            timeLabel = date.getHours() + ":" + date.getMinutes();
+            calendar = Calendar.getInstance();
+            calendar.setTimeInMillis(this.time);
+            timeLabel = String.format("%d月%d日 %d:%d",
+                    calendar.get(Calendar.MONTH) + 1,
+                    calendar.get(Calendar.DAY_OF_MONTH),
+                    calendar.get(Calendar.HOUR_OF_DAY),
+                    calendar.get(Calendar.MINUTE));
         }
 
         public long getTime() {
